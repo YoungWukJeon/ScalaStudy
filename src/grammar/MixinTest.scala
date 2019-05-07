047004a36ae00bc4f57f5e699a2cfa3c25fd55c5
kani.jeon <kani.jeon@kakaocorp.com>
2019-05-07 14:15:36 +0900

    MixinTest

---
+++ src/grammar/MixinTest.scala
@@ -0,0 +1,35 @@
+package grammar
+
+abstract class AbsIterator {
+  type T
+  def hasNext: Boolean
+  def next: T
+}
+
+trait RichIterator extends AbsIterator {
+  def foreach(f: T => Unit) = {
+    while (hasNext) {
+      f(next)
+    }
+  }
+}
+
+class StringIterator(s: String) extends AbsIterator {
+  type T = Char
+  private var i = 0
+  def hasNext = i < s.length()
+  def next = {
+    val ch = s charAt i
+    i += 1
+    ch
+  }
+}
+
+object StringIteratorTest {
+  def main(args: Array[String]): Unit = {
+    val sample = "Hello, world!"
+    class Iter extends StringIterator(sample) with RichIterator
+    val iter = new Iter
+    iter foreach println
+  }
+}
