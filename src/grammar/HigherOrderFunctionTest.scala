69cda4b1c21385b2598862b357f64afc3246809d
kani.jeon <kani.jeon@kakaocorp.com>
2019-05-13 09:30:51 +0900

    HigherOrderFunctionTest

---
+++ src/grammar/HigherOrderFunctionTest.scala
@@ -0,0 +1,11 @@
+package grammar
+
+class Decorator(left: String, right: String) {
+  def layout[A] (x: A) = left + x.toString + right
+}
+
+object HigherOrderFunctionTest extends App {
+  def apply(f: Int => String, v: Int) = f(v)
+  val decorator = new Decorator("[", "]")
+  println(apply(decorator.layout, 7))
+}
